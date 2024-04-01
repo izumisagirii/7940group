64932fb94966cb4122fdc7b1f0f9907d1e4157e6
FelixyZhu@example.com <FelixyZhu@example.com>
2024-04-01 15:21:56 +0800

    api_key test

--- chatbot.py
+++ chatbot.py
@@ -165,7 +165,7 @@ def yelp_in_bot(update: Update, context: CallbackContext) -> None:
             return
 
         # Use the yelp module to search for businesses based on the location and type parameters
-        response = yelp.search(api_key=KJw1YCF5WJzMMwV458YOCd3KRTMhUHjC7SZ5tv24vHKVBWZsDFlm3z9DVyRvtA6_0xMTwnDRUBqPq9od8JVBEp3363UFnvgtKLC0D4pD-FcSuLWvrQvaPgTwcZj4ZXYx,
+        response = yelp.search(api_key='KJw1YCF5WJzMMwV458YOCd3KRTMhUHjC7SZ5tv24vHKVBWZsDFlm3z9DVyRvtA6_0xMTwnDRUBqPq9od8JVBEp3363UFnvgtKLC0D4pD-FcSuLWvrQvaPgTwcZj4ZXYx',
                                 term=type_parameter, location=location_parameter)
         businesses = response.get('businesses', [])
 
