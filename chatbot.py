865b8d72f696ae22872b0ce77f58af7d245d23f5
FelixyZhu@example.com <FelixyZhu@example.com>
2024-04-01 15:17:53 +0800

    api key test

--- chatbot.py
+++ chatbot.py
@@ -141,7 +141,7 @@ def yelp_in_bot(update: Update, context: CallbackContext) -> None:
 
     Args:
         update (Update): This update is a message update.
-        context (CallbackContext): The context of the bot’s conversation with the user.
+        context (CallbackContext): The context of the bot's conversation with the user.
 
     Returns:
         None
@@ -165,7 +165,7 @@ def yelp_in_bot(update: Update, context: CallbackContext) -> None:
             return
 
         # Use the yelp module to search for businesses based on the location and type parameters
-        response = yelp.search(api_key='KJw1YCF5WJzMMwV458YOCd3KRTMhUHjC7SZ5tv24vHKVBWZsDFlm3z9DVyRvtA6_0xMTwnDRUBqPq9od8JVBEp3363UFnvgtKLC0D4pD-FcSuLWvrQvaPgTwcZj4ZXYx',
+        response = yelp.search(api_key=KJw1YCF5WJzMMwV458YOCd3KRTMhUHjC7SZ5tv24vHKVBWZsDFlm3z9DVyRvtA6_0xMTwnDRUBqPq9od8JVBEp3363UFnvgtKLC0D4pD-FcSuLWvrQvaPgTwcZj4ZXYx,
                                 term=type_parameter, location=location_parameter)
         businesses = response.get('businesses', [])
 
